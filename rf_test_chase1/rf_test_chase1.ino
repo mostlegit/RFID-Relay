8d70a2779c64da2b62483182eab425b26f29849a
Chase S <chase@mostlegit.com>
2017-05-28 14:28:56 -0700

    Add files via upload
    Transmit 1 and 0 basic test

---
+++ rf_test_chase1/rf_test_chase1.ino
@@ -0,0 +1,27 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+RH_ASK driver;
+
+void setup()
+{
+    Serial.begin(9600); // Debugging only
+    if (!driver.init())
+         Serial.println("init failed");
+}
+
+void loop()
+{
+    uint8_t buf[1];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+      int i;
+      // Message with a good checksum received, dump it.
+      Serial.print("GPIO ON: ");
+      char *output = (char*)buf;
+      Serial.println(output);  
+      Serial.println((char*)buf);
+      
+    }
+}
